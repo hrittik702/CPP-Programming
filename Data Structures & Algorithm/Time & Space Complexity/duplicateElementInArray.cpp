2ffbbc8af2c0fdd2e9c0de7c02d55eedd2bed433
hrittik702 <mmhrittik@gmail.com>
2025-10-07 09:41:52 +0530

    approach by using another vector/array

--- Data Structures & Algorithm/Time & Space Complexity/duplicateElementInArray.cpp
+++ Data Structures & Algorithm/Time & Space Complexity/duplicateElementInArray.cpp
@@ -15,27 +15,28 @@ int main(){
     int n=7;
     int arr[n+1] = {2,5,4,3,7,6,3,1};
     // duplicate element
+
     // // M-1 Brute force method
-    // bool flag=false;
-    // for(int i=0; i<n+1; i++){
-    //     for(int j=i+1; j<n+1; j++){
-    //         if(arr[j]==arr[i]){
-    //             cout<<"Duplicate element : "<<arr[j]<<endl;
-    //             flag = true;
-    //             break;
-    //         }
-    //     }
-    //     if(flag==true) break;
-    // }
+    bool flag=false;
+    for(int i=0; i<n+1; i++){
+        for(int j=i+1; j<n+1; j++){
+            if(arr[j]==arr[i]){
+                cout<<"Duplicate element : "<<arr[j]<<endl;
+                flag = true;
+                break;
+            }
+        }
+        if(flag==true) break;
+    }
 
     //M-2 - Using Math's to find the duplicate element
-    // int s1=(7*(7+1))/2;
-    // int s2=0;
-    // for(int i=0; i<=n;i++){
-    //     s2+=arr[i];
-    // } 
-    // int duplicate = s2-s1;
-    // cout<<"Duplicate element : "<<duplicate<<endl;
+    int s1=(7*(7+1))/2;
+    int s2=0;
+    for(int i=0; i<=n;i++){
+        s2+=arr[i];
+    } 
+    int duplicate = s2-s1;
+    cout<<"Duplicate element : "<<duplicate<<endl;
     
     //M-3 - using another array or vector
     vector <int> array(8,0);
@@ -46,7 +47,6 @@ int main(){
             array.at((arr[i]))=1;
         }
     }
-    
     
     auto stop = high_resolution_clock :: now();
     auto duration = duration_cast<microseconds>(start-stop);
